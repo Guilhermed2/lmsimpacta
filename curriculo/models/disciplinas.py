899931398ffa5732da629a0fc38ff429bcddc84f
Guilherme Dias Martins <1801611@SALAS.AULAS>
2019-05-15 22:18:35 -0300

    alterações em sala 15-05

--- curriculo/models/disciplinas.py
+++ curriculo/models/disciplinas.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 from contas.models import Coordenador
 
@@ -26,3 +27,13 @@ class Disciplina(models.Model):
 
     def __str__(self):
         return self.nome
+
+    def clean(self):
+        if self.percentual_teorico > 100 or self.percentual_teorico < 0:
+            raise ValidationError('O percentual Teórico está deve estar entre 0 e 100.')
+
+        if self.percentual_pratico > 100 or self.percentual_pratico < 0:
+            raise ValidationError('O percentual Prático está deve estar entre 0 e 100.')
+
+        if self.percentual_pratico + self.percentual_teorico != 100:
+            raise ValidationError('A soma dos percentuais Teórico e Prático não pode ser maior que 100.')
